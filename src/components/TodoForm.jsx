83f1d072506e855c34b4e0edaf0168d425a51073
Faraz shafi <farazpachu777@gmail.com>
2024-09-18 10:03:02 +0530

    added validation data

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const TodoForm = ({todo}) => {
-    const [input,setInput] = useState("")
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        todo(input)
-        setInput(" ")
+const TodoForm = ({ todo }) => {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmedInput = input.trim();
+
+    if (trimmedInput.length > 1) {
+      todo(trimmedInput);
+      setInput("");
+    } else {
+      setInput("");
     }
+  };
+
   return (
     <>
-        <h1>TASKS</h1>
-        <form className='TodoForm'>
-            <input value={input} onChange={(e)=>setInput(e.target.value)} className='todo-input' type="text" placeholder="what is the task" />
-            <button onClick={handleSubmit} className='todo-btn'>Add Task</button>
-        </form>
+      <h1>TASKS</h1>
+      <form className="TodoForm">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="todo-input"
+          type="text"
+          placeholder="What is the task?"
+        />
+        <button onClick={handleSubmit} className="todo-btn">
+          Add Task
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
